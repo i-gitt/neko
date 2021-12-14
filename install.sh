272e69d7c90dda5fa3f3127af20b833714409bb5
In The Kode <mail4al@web.de>
2021-12-14 01:01:36 +0100

    removed Qstat

--- install.sh
+++ install.sh
@@ -4,13 +4,3 @@ python3 -m venv env
 ./env/bin/pip install --upgrade pip
 ./env/bin/pip install -r requirements.txt
 
-git clone https://github.com/multiplay/qstat
-cd qstat
-./autogen.sh
-./configure
-make
-
-cd ..
-mv qstat qstat-1
-cp qstat-1/qstat .
-rm -rf qstat-1
