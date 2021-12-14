4203f6b88dac7175e5dd26c60fec50794afa24b0
In The Kode <mail4al@web.de>
2021-12-14 01:14:54 +0100

    added call without screen

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/usr/bin/screen -m  -S neko /home/oa/neko/env/bin/python /home/oa/neko/neko.py
+#/usr/bin/screen -m  -S neko /home/oa/neko/env/bin/python /home/oa/neko/neko.py
+/home/oa/neko/env/bin/python /home/oa/neko/neko.py 2>1 /dev/null &
